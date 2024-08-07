ecd29b4760836c27b34ba34787841e5ebfe5f6ee
Francis <51071976+munch7@users.noreply.github.com>
2024-08-07 16:57:43 +0300

    2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { routes } from './app.routes';
 
@@ -9,6 +10,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     importProvidersFrom(),
-    provideAnimationsAsync(), provideAnimationsAsync(),
+    provideAnimationsAsync(), 
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ]
 };
